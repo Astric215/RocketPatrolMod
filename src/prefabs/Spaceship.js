3d6058931e0577a88cfe83545228f9376ff51a38
Astric215 <davidiwd215@gmail.com>
2022-04-17 13:33:59 -0700

    moving old files

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,23 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+   constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene,x,y,texture,frame);
+
+      //add object ot existing scene
+      scene.add.existing(this);
+      this.points = pointValue;
+      this.moveSpeed = game.settings.spaceshipSpeed;
+   }
+
+   update() {
+      //left/Right
+      this.x -= this.moveSpeed;
+      //wrap around from left to right
+      if(this.x <= 0 - this.width) {
+         this.x = game.config.width;
+      }
+   }
+
+   reset() {
+      this.x = game.config.width;
+   }
+}
